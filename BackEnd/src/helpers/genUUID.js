6c291480d38e843cfda176af61a678c9b9f67878
geek2tech <geek2tech@geek2tech.Fr>
2023-10-11 16:16:04 +0200

    BACKEND - Add genuuid feature

---
+++ BackEnd/src/helpers/genUUID.js
@@ -0,0 +1,19 @@
+const { v4: uuidV4 } = require('uuid')
+const logger = require('../services/Logger')
+
+function genUUID(){
+
+    const v4Options = {
+        offset:10
+    }
+
+    logger.log({
+        level:'info',
+        module:'genUUID',
+        message:'Call genUUID'
+    })
+
+    const uuid = uuidV4(v4Options)
+    return uuid
+}
+
