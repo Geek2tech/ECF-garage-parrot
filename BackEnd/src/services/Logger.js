b9ea2ef82b3695f9ad136f025b46a08cb127de28
geek2tech <geek2tech@geek2tech.Fr>
2023-10-11 17:24:27 +0200

    BACKEND - change logger option to use timestamp format

--- BackEnd/src/services/Logger.js
+++ BackEnd/src/services/Logger.js
@@ -1,37 +1,31 @@
 const winston = require('winston')
 const dailyRotateFile = require('winston-daily-rotate-file')
-const now = new Date()
-let minute = now.getMinutes()
-minute = minute.toString().padStart(2,0)
-const time = now.getHours() + ":" + minute+ ':' + now.getSeconds()
-const logTime = winston.format(info => {
-    info.time = time
-    return info
-})
 
 const logger = winston.createLogger({
-    level:'info',
+    level: 'info',
     format: winston.format.combine(
-        logTime(),
+        winston.format.timestamp({
+            format: 'HH:mm:ss'
+        }),
         winston.format.simple()
     ),
-    exitOnError:false,
+    exitOnError: false,
 
     transports: [
         new dailyRotateFile({
-            filename : './log/error-%DATE%.log',
-            datePattern:'YYYY-MM-DD',
-            zippedArchive :true,
-            maxSize:'20m',
-            maxFiles :'14d',
-            level:'error'
+            filename: './log/error-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+            level: 'error'
         }),
         new dailyRotateFile({
-            filename : './log/combined-%DATE%.log',
-            datePattern:'YYYY-MM-DD',
-            zippedArchive :true,
-            maxSize:'20m',
-            maxFiles :'14d',
+            filename: './log/combined-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
 
         }),
 
@@ -40,9 +34,9 @@ const logger = winston.createLogger({
 
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
-        format:winston.format.combine(
+        format: winston.format.combine(
             winston.format.colorize({
-                all:true
+                all: true
             })
         )
     }))
