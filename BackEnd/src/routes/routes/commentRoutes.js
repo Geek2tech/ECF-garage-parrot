1ed46ec11849201fef21ac4c82beeccbd7fe5509
geek2tech <geek2tech@geek2tech.Fr>
2023-10-13 11:23:07 +0200

    BACKEND - Add comments CRUD feature

--- BackEnd/src/routes/routes/commentRoutes.js
+++ BackEnd/src/routes/routes/commentRoutes.js
@@ -3,7 +3,14 @@ const commentController = require("../../controller/Comment.Controller")
 const router = express.Router()
 
 // get validated comment
-router.get('/commentsvalidated',commentController.getValidatedComment)
+router.get('/comments',commentController.getValidatedComment)
 
+// get unvalidated comment
+router.get('/protected/commentsPending',commentController.getUnvalidatedComment)
+
+// Add comment
+router.post('/comment',commentController.addComment)
+// Delete comment
+router.delete('/comment',commentController.deleteComment)
 
 module.exports = router
