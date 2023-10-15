8f6ce91156deb5eb30e145e3f9a72d5baf0b1a8f
geek2tech <geek2tech@geek2tech.Fr>
2023-10-15 19:08:03 +0200

    BACKEND - add try catch block in all controller

--- BackEnd/src/routes/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const authController = require("../../controller/Auth.controller")
-const router = express.Router()
-
-// Login request
-router.post('/login', authController.authentification)
-
-
-module.exports = router
--- BackEnd/src/routes/routes/mailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const mailController = require("../../controller/Mail.Controller");
-const router = express.Router()
-
-
- // send mail
-router.post("/mail", mailController.sendMail)
-
-module.exports = router
