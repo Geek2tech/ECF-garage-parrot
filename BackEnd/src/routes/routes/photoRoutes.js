19efaebd35a8c0820b9cf3e2d81fa705793703a5
geek2tech <geek2tech@geek2tech.Fr>
2023-10-17 12:02:09 +0200

    BACKEND - re add delete photo route

--- BackEnd/src/routes/routes/photoRoutes.js
+++ BackEnd/src/routes/routes/photoRoutes.js
@@ -10,6 +10,8 @@ router.get('/primary/:car_id',photoController.getPrimaryPhoto)
 router.post("/protected/photo/:car_id/:primary", photoController.addPhoto)
 //Download a file
 router.get("/photo/:name", photoController.downloadPhoto)
+// Delete car photo
+router.delete('/protected/photo/:car_id')
 
 
 
