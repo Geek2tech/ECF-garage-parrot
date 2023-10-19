eafc0b33f4667751bf238cb7a8e5a5a12a14b982
geek2tech <geek2tech@geek2tech.Fr>
2023-10-19 22:59:40 +0200

    BACKEND - modify getCars function to use carID for all cars or specifique one

--- BackEnd/src/controller/Car.Controller.js
+++ BackEnd/src/controller/Car.Controller.js
@@ -3,223 +3,225 @@ const database = require('../services/db')
 const paginatedSelectQuery = require('../helpers/paginatedSelectQuery')
 const {suppressSpecialChar} = require("../helpers/fieldControl")
 const photoController = require('../controller/Photos.Controller')
-const deleteItem = require('../helpers/deleteItem')
-async function getCars (req,res) {
-try {
-    const priceFilter = suppressSpecialChar(req.body.priceFilter)
-    const circulationYearFilter = suppressSpecialChar(req.body.circulationYearFilter)
-    const mileageFilter = suppressSpecialChar(req.body.mileageFilter)
 
 
-    logger.log({
-        level:'info',
-        module:'Cars',
-        message:'Call getCars'
-    })
+async function getCars(req, res) {
+    try {
+        const priceFilter = suppressSpecialChar(req.body.priceFilter)
+        const circulationYearFilter = suppressSpecialChar(req.body.circulationYearFilter)
+        const mileageFilter = suppressSpecialChar(req.body.mileageFilter)
+        const carId = suppressSpecialChar(req.body.car_id)
 
+        logger.log({
+            level: 'info',
+            module: 'Cars',
+            message: 'Call getCars'
+        })
+if(carId === "*"){
     query = "SELECT * FROM car_view WHERE circulation_year >= " + circulationYearFilter + " and price <= " + priceFilter + " and mileage <= " + mileageFilter
 
-    paginatedSelectQuery(req,res,query)
-}catch (err) {
-
-    logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Internal error ${err}`
-    })
+}else {
+    query = "SELECT * FROM car_view WHERE car_id = " +carId
 
 }
 
+        paginatedSelectQuery(req, res, query)
+    } catch (err) {
+
+        logger.log({
+            level: 'info',
+            module: 'Cars',
+            message: `Internal error ${err}`
+        })
+
+    }
+
 
 }
 
-async function addCar(req,res) {
+async function addCar(req, res) {
 
     const car = {
-        price:suppressSpecialChar(req.body.price),
-        circulation_year:(req.body.circulation_year),
-        mileage:suppressSpecialChar(req.body.mileage),
-        horse_power:suppressSpecialChar(req.body.horse_power),
-        fiscal_power:suppressSpecialChar(req.body.fiscal_power),
-        doors:suppressSpecialChar(req.body.doors),
-        cylinder_capacity:suppressSpecialChar(req.body.cylinder_capacity),
-        motor_type:suppressSpecialChar(req.body.motor_type),
-        model_name:suppressSpecialChar(req.body.model_name),
-        color:suppressSpecialChar(req.body.color),
-        fuel_id:suppressSpecialChar(req.body.fuel_id),
-        towing_id:suppressSpecialChar(req.body.towing_id),
-        transmission_id:suppressSpecialChar(req.body.transmission_id),
-        constructor_id:suppressSpecialChar(req.body.constructor_id)
-    }
-
-logger.log({
-    level:'info',
-    module:'Cars',
-    message:`Call addCar }`
-})
-    const query = `INSERT INTO cars (price, circulation_year, mileage, horse_power,fiscal_power, doors, cylinder_capacity, motor_type,
-                               model_name, color, fuel_id, towing_id, transmission_type_id, constructor_id)
-             VALUES (?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?)`
-
-    logger.log({
-    level:'info',
-        module:'Cars',
-        message:'Bdd Request'
-    }
-)
-
-database.dbconnect.query(query,[
-    car.price,
-    car.circulation_year,
-    car.mileage,
-    car.horse_power,
-    car.fiscal_power,
-    car.doors,
-    car.cylinder_capacity,
-    car.motor_type,
-    car.model_name,
-    car.color,
-    car.fuel_id,
-    car.towing_id,
-    car.transmission_id,
-    car.constructor_id
-],(err,result) => {
-
-    if(err) {
-        logger.log({
-            level:'error',
-            module:'Cars',
-            message:`Sql Error : ${err.message}`
-        })
-        return res.status(500).send(`Sql error : ${err.message}`)
+        price: suppressSpecialChar(req.body.price),
+        circulation_year: (req.body.circulation_year),
+        mileage: suppressSpecialChar(req.body.mileage),
+        horse_power: suppressSpecialChar(req.body.horse_power),
+        fiscal_power: suppressSpecialChar(req.body.fiscal_power),
+        doors: suppressSpecialChar(req.body.doors),
+        cylinder_capacity: suppressSpecialChar(req.body.cylinder_capacity),
+        motor_type: suppressSpecialChar(req.body.motor_type),
+        model_name: suppressSpecialChar(req.body.model_name),
+        color: suppressSpecialChar(req.body.color),
+        fuel_id: suppressSpecialChar(req.body.fuel_id),
+        towing_id: suppressSpecialChar(req.body.towing_id),
+        transmission_id: suppressSpecialChar(req.body.transmission_id),
+        constructor_id: suppressSpecialChar(req.body.constructor_id)
     }
 
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Insert successfully with id : ${result.insertId}`
+        level: 'info',
+        module: 'Cars',
+        message: `Call addCar }`
     })
+    const query = `INSERT INTO cars (price, circulation_year, mileage, horse_power, fiscal_power, doors,
+                                     cylinder_capacity, motor_type,
+                                     model_name, color, fuel_id, towing_id, transmission_type_id, constructor_id)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-    return res.status(200).send(`Ajout ok id : ${result.insertId}`)
+    logger.log({
+            level: 'info',
+            module: 'Cars',
+            message: 'Bdd Request'
+        }
+    )
+
+    database.dbconnect.query(query, [
+        car.price,
+        car.circulation_year,
+        car.mileage,
+        car.horse_power,
+        car.fiscal_power,
+        car.doors,
+        car.cylinder_capacity,
+        car.motor_type,
+        car.model_name,
+        car.color,
+        car.fuel_id,
+        car.towing_id,
+        car.transmission_id,
+        car.constructor_id
+    ], (err, result) => {
+
+        if (err) {
+            logger.log({
+                level: 'error',
+                module: 'Cars',
+                message: `Sql Error : ${err.message}`
+            })
+            return res.status(500).send(`Sql error : ${err.message}`)
+        }
+
+        logger.log({
+            level: 'info',
+            module: 'Cars',
+            message: `Insert successfully with id : ${result.insertId}`
+        })
+
+        return res.status(200).send(`Ajout ok id : ${result.insertId}`)
 
 
-})}
+    })
+}
 
 
-async function deleteCar(req,res) {
+async function deleteCar(req, res) {
 
     const car_id = suppressSpecialChar(req.params.car_id)
 
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Call deleteCar with params ${car_id}`
+        level: 'info',
+        module: 'Cars',
+        message: `Call deleteCar with params ${car_id}`
     })
 
     // Suppress the car-equipement relation
 
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:'Bdd resquest to suppress the car-equipements '
+        level: 'info',
+        module: 'Cars',
+        message: 'Bdd resquest to suppress the car-equipements '
     })
 
-    let query = `DELETE FROM cars_equipements WHERE car_id = ?`
+    let query = `DELETE
+                 FROM cars_equipements
+                 WHERE car_id = ?`
 
-    database.dbconnect.query(query,[car_id],(err) => {
-        if(err) {
+    database.dbconnect.query(query, [car_id], (err) => {
+        if (err) {
             logger.log({
-                level:'error',
-                module:'Cars',
-                message:`Sql Error : ${err.sqlMessage}`
+                level: 'error',
+                module: 'Cars',
+                message: `Sql Error : ${err.sqlMessage}`
             })
             return res.status(500).send(`Sql error : ${err.sqlMessage}`)
         }
 
         logger.log({
-            level:'info',
-            module:'Cars',
-            message:`Suppress photo for the car id ${car_id}`
+            level: 'info',
+            module: 'Cars',
+            message: `Suppress photo for the car id ${car_id}`
         })
         // Suppress the photo file and bdd records
 
-        photoController.deletePhotos(req,res)
+        photoController.deletePhotos(req, res)
         // Suppress the car
 
         logger.log({
-            level:'info',
-            module:'Cars',
-            message:'Bdd request for suppress car'
+            level: 'info',
+            module: 'Cars',
+            message: 'Bdd request for suppress car'
         })
 
         query = " DELETE FROM cars WHERE car_id = ?"
-        database.dbconnect.query(query,[car_id],(err,result) => {
-            if(err) {
+        database.dbconnect.query(query, [car_id], (err, result) => {
+            if (err) {
                 logger.log({
-                    level:'error',
-                    module:'Cars',
-                    message:`Sql error : ${err.sqlMessage}`
+                    level: 'error',
+                    module: 'Cars',
+                    message: `Sql error : ${err.sqlMessage}`
                 })
                 return res.status(500).send(`Sql error ${err.sqlMessage}`)
             }
             logger.log({
-                level:'info',
-                module:'Cars',
-                message:`Suppress car ${car_id} successfully`
+                level: 'info',
+                module: 'Cars',
+                message: `Suppress car ${car_id} successfully`
             })
-return res.status(200).send('Suppression ok')
+            return res.status(200).send('Suppression ok')
         })
 
     })
 
 
-
-
-
-
-
-
-
-
 }
 
-async function getPriceMinMax(req,res) {
+async function getPriceMinMax(req, res) {
 
     const query = 'SELECT MIN(price) as min , MAX(price) as max FROM car_view'
 
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Call getMaxPrice`
+        level: 'info',
+        module: 'Cars',
+        message: `Call getMaxPrice`
     })
 
-    paginatedSelectQuery(req,res,query)
+    paginatedSelectQuery(req, res, query)
 }
 
-async function getCirculationYearMinMax(req,res) {
+async function getCirculationYearMinMax(req, res) {
 
     const query = 'SELECT MIN(circulation_year) as min , MAX(circulation_year) as max FROM car_view'
 
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Call getMaxCirculationYear`
+        level: 'info',
+        module: 'Cars',
+        message: `Call getMaxCirculationYear`
     })
 
-    paginatedSelectQuery(req,res,query)
+    paginatedSelectQuery(req, res, query)
 
 }
 
-async function getMileageMinMax(req,res) {
+async function getMileageMinMax(req, res) {
 
     const query = 'SELECT MIN(mileage) as min,MAX(mileage) as max FROM car_view'
     logger.log({
-        level:'info',
-        module:'Cars',
-        message:`Call getMaxMileage`
+        level: 'info',
+        module: 'Cars',
+        message: `Call getMaxMileage`
     })
 
-    paginatedSelectQuery(req,res,query)
+    paginatedSelectQuery(req, res, query)
 
 }
 
