ac5d7f215576f054d068b5c9a673988fa2b87889
geek2tech <geek2tech@geek2tech.Fr>
2023-11-02 12:03:25 +0100

    BACKEND change res.status for login FRONTEND add login feature

--- BackEnd/src/routes/routes/transmissionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const transmissionController = require("../../controller/Transmission.Controller")
-const router = express.Router()
-
-// Get list of transmission type
-router.get('/api/transmissions', transmissionController.getTransmission)
-
-
-module.exports = router
